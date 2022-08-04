7c2fdaa267934ff949fbb206223a7c2ed74b85ec
j9poi <96616092+j9poi@users.noreply.github.com>
2022-08-04 10:44:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("Jeff's Kitchen')
+streamlit.title("Jeff's Kitchen")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')

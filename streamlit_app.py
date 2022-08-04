9ab3faf3ef90f6625088bdec800bbc3a1d472eeb
j9poi <96616092+j9poi@users.noreply.github.com>
2022-08-04 11:04:56 +0200

    Update streamlit_app.py
    Added additional text and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 
 streamlit.title("Jeff's Kitchen")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free Range Egg')            
+
+streamlit.header('Breakfast Healthy Options')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
                 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
     

8d95b291e76c4887b9393a9c131af540c7316466
j9poi <96616092+j9poi@users.noreply.github.com>
2022-08-09 11:54:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-
+import snowflake.connector
 
 streamlit.title("Jeff's Kitchen")
 
@@ -38,5 +38,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
 

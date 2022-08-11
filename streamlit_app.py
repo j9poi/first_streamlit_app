2bd3f900d84d321334bf510760eead5e6880f04e
j9poi <96616092+j9poi@users.noreply.github.com>
2022-08-11 10:33:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
 
 #import requests
 #New section to display fruityvice api response
-        fruityvice_response  = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #Take the json version of the response and normalize it
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
